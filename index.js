6f670dfa02d8f98277181da6300d58ba3860ac8d
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-08-18 15:54:05 +0100

    toggle between shop cart and product count

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const show_pricing = document.querySelectorAll(".shop");
+const priceIncrease = document.querySelectorAll(".quantity")
+
+const carts = [0, 0 , 0 , 0 , 0, 0, 0]
+
 
 for(let i=0; i<show_pricing.length; i++){
     show_pricing[i].addEventListener("click", (event)=>{
         event.preventDefault();
         // alert("hello world!");
         show_pricing[i].classList.add("close");
+        priceIncrease[i].classList.remove("close");
+        carts[i]++;
         
-
+        
     })
 }
