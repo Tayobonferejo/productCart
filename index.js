d61b5806e0d4c23801547c2ae40839f09c1870de
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-08-19 17:41:45 +0100

    more content added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const show_pricing = document.querySelectorAll(".shop");
 const priceIncrease = document.querySelectorAll(".quantity")
+const counterItem = document.querySelectorAll(".counter");
 
 const carts = [0, 0 , 0 , 0 , 0, 0, 0]
 
@@ -11,7 +12,6 @@ for(let i=0; i<show_pricing.length; i++){
         show_pricing[i].classList.add("close");
         priceIncrease[i].classList.remove("close");
         carts[i]++;
-        
         
     })
 }
