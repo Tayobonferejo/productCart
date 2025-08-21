22e950d67fadc80b221eae46b2a26a0c81f764b8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-08-21 14:35:08 +0100

    decrement added functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const show_pricing = document.querySelectorAll(".shop");
 const priceIncrease = document.querySelectorAll(".quantity")
 const counterItem = document.querySelectorAll(".counter");
 const increment = document.querySelectorAll(".increaser");
+const decrement = document.querySelectorAll(".decreaser");
 
 const carts = [0, 0 , 0 , 0 , 0, 0, 0]
 
@@ -14,12 +15,32 @@ show_pricing.forEach(function(item, i)
             priceIncrease[i].classList.remove("close");
             carts[i]++;
             counterItem[i].innerText = carts[i];
-            increment[i].addEventListener("click" ,function(event){
-                event.preventDefault();
-                carts[i]++;
-                counterItem[i].innerText = carts[i];
-            })
+
         }
+
+    });
+
+    increment[i].addEventListener("click" ,function(event){
+            event.preventDefault();
+            carts[i]++;
+            counterItem[i].innerText = carts[i];
+
     })
+
+    decrement[i].addEventListener("click" , function(event){
+        event.preventDefault();
+
+            if (carts[i] === 1) {
+                priceIncrease[i].classList.add("close");
+                item.classList.remove("close");
+            }
+
+            if(carts[i] > 0) {
+                carts[i]--;
+                counterItem[i].innerText = carts[i];
+            }
+
+    })
+
 })
 
